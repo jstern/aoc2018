e85f8ad3fb202917e1858ce81e31cb064173cc8f
Jon Stern <jstern@leapfrogonline.com>
2018-12-30 22:29:40 -0600

    deque the halls

--- aoc2018/marbles.py
+++ aoc2018/marbles.py
@@ -1,26 +1,17 @@
+from collections import deque
+
+
 class MarbleGame:
     def __init__(self, players: int, hi: int):
         self.players = players
         self.hi = hi
-        self.circle = [[0]]
+        self.circle = deque([0])
         self.scores = [0] * players
         self.turn = 1
         self.player = 0
-        self.current = 0
-
-    def size(self):
-        return len(self.circle[0])
-
-    def insert(self, i, val):
-        self.circle[0].insert(i, val)
-
-    def append(self, val):
-        self.circle[0].append(val)
-
-    def pop(self, i):
-        return self.circle[0].pop(i)
 
     def play_turn(self):
+        # invariant: current marble is always at the end of the deque
         if self.turn % 23 == 0:
             self.play_23()
         else:
@@ -28,40 +19,21 @@ class MarbleGame:
         self.turn = self.turn + 1
         self.player = (self.player + 1) % self.players
 
+    def play_normal(self):
+        # player places marble between 1 clockwise and 2 clockwise of current
+        self.circle.rotate(-1)
+        self.circle.append(self.turn)
+
     def play_23(self):
         # player pockets the marble
         self.scores[self.player] = self.scores[self.player] + self.turn
-
-        # player removes and pockets the marble 7 counterclockwise
-        idx = (self.current - 7) % self.size()
-        # pop is O(k)
-        self.scores[self.player] = self.scores[self.player] + self.pop(idx)
-        self.current = idx
-
-    def play_normal(self):
-        # player places marble between 1 clockwise and 2 clockwise of current
-        c1 = (self.current + 1) % self.size()
-        c2 = (c1 + 1) % self.size()
-        if c2 <= c1:
-            self.append(self.turn)
-            self.current = c1 + 1
-        else:
-            self.insert(c2, self.turn)
-            self.current = c2
+        self.circle.rotate(7)
+        self.scores[self.player] = self.scores[self.player] + self.circle.pop()
+        self.circle.rotate(-1)
 
     def play(self):
         while self.turn <= self.hi:
             self.play_turn()
-
-    @property
-    def state(self):
-        marbles = [str(m) for m in self.circle]
-        marbles[self.current] = f"({marbles[self.current]})"
-        return f"[{self.player}]  {' '.join(marbles)}"
-
-    @property
-    def current_marble(self):
-        return self.circle[self.current]
 
     @property
     def winner(self):
